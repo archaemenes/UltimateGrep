7529ba03c502f22eb907bbcb12af1e8e47e79df0
Anisha Mhatre <50442176+archaemenes@users.noreply.github.com>
2023-12-26 23:50:44 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
-git clone https://github.com/archaemenes/UltimateGrepDir.git
-cd UltimateGrepDir
+git clone https://github.com/archaemenes/UltimateGrep.git
+cd UltimateGrep
 mv UltimateGrep.sh UltimateGrep
 chmod +x UltimateGrep 
 echo "export PATH=\$PATH:`pwd`/"
 echo "export PATH=\$PATH:`pwd`/" >> ~/.bashrc && source ~/.bashrc
-
+echo " "
+echo "Setup Done"
